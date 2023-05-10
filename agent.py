10ddc5708d693040450853ecf3ee195a4862664b
Harper Reed <harper@nata2.org>
2023-05-09 21:07:54 -0500

    refactored agent

--- agent.py
+++ agent.py
@@ -45,19 +45,13 @@ class HouseBot:
         logging.info(response)
         return response
 
-
-class MessageBatcher:
+class AgentListener:
     def __init__(self, client, timeout):
 
         self.logger = logging.getLogger(__name__)
-        self.message_queue = Queue()
-        self.last_received_timestamp = None
-        self.batch_start_time = None
-        self.timeout = timeout
         self.stopped = False
         self.client = client
         self.house_bot = HouseBot()
-        self.last_batch_messages = None
 
     def on_message(self, client, userdata, msg):
         try:
@@ -79,40 +73,6 @@ class MessageBatcher:
 
         topic = os.getenv('NOTIFICATION_TOPIC', 'your/input/topic/here')
         self.client.publish(topic, response)
-
-
-
-
-    def send_batched_messages(self):
-        batch = []
-        while not self.message_queue.empty():
-            try:
-                batch.append(self.message_queue.get_nowait())
-            except Empty:
-                break
-
-        if batch:
-            self.logger.info(f"Sending batched messages: {batch}")
-            output = {'messages': batch}
-            json_output = json.dumps(output)
-            
-
-            response = self.house_bot.generate_response(json_output, self.last_batch_messages)
-            self.last_batch_messages = json_output
-
-            topic = os.getenv('NOTIFICATION_TOPIC', 'your/input/topic/here')
-            self.client.publish(topic, response)
-
-            # Log the sent batched messages at INFO level
-            self.logger.info(f"Sent batched messages: {json_output}")
-
-        self.batch_start_time = None
-
-    def run(self):
-        while not self.stopped:
-            if self.last_received_timestamp and (time.time() - self.last_received_timestamp) >= self.timeout:
-                self.send_batched_messages()
-            time.sleep(0.1)
 
     def stop(self):
         self.stopped = True
@@ -139,16 +99,9 @@ keep_alive_interval = int(os.getenv('MQTT_KEEP_ALIVE_INTERVAL', 60))
 client.connect(broker_address, port_number, keep_alive_interval)
 logging.debug(f"Connected to MQTT broker at {broker_address}:{port_number}")
 
-message_batcher = MessageBatcher(client, timeout)
+message_batcher = AgentListener(client, timeout)
 
 client.loop_start()
-
-try:
-    message_batcher.run()
-except KeyboardInterrupt:
-    logging.info("Shutting down...")
-    message_batcher.stop()
-
 client.loop_stop()
 client.disconnect()
 
