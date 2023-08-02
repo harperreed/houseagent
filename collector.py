3e1a858f16b7fe01996d30d5424680d0a2d6ccc3
Harper Reed <harper@nata2.org>
2023-08-02 15:33:42 -0500

    fixed bug

--- collector.py
+++ collector.py
@@ -6,25 +6,26 @@ import structlog
 from houseagent.message_batcher import MessageBatcher
 
 # Load configuration from .env file
-from dotenv import load_dotenv
 load_dotenv()
-
 
 logger = structlog.get_logger(__name__)
 
 def on_connect(client, userdata, flags, rc):
+    logger.info("Connected to MQTT broker")
     topic = os.getenv('SUBSCRIBE_TOPIC', 'your/input/topic/here')
     client.subscribe(topic)
+    logger.debug(f"Subscribing to topic: {topic}")
     logger.info(f"Connected with result code {rc}. Subscribed to topic: {topic}")
 
 def on_message(client, userdata, msg):
+    logger.info("Received message")
+    logger.debug(f"Message: {msg.payload}")
     message_batcher.on_message(client, userdata, msg)
 
 def on_disconnect(client, userdata, rc):
     logger.info("Disconnected from MQTT broker")
     if rc != 0:
         logger.error(f"Unexpected disconnection. Result code: {rc}")
-  
 
 timeout = int(os.getenv('TIMEOUT', 60))
 
@@ -35,7 +36,7 @@ client.on_disconnect = on_disconnect
 
 broker_address = os.getenv('MQTT_BROKER_ADDRESS', 'localhost')
 port_number = int(os.getenv('MQTT_PORT', 1883))
-keep_alive_interval = int(os.getenv('MQTT_KEEP_ALIVE_INTERVAL', 120))
+keep_alive_interval = int(os.getenv('MQTT_KEEP_ALIVE_INTERVAL', 60))
 
 client.connect(broker_address, port_number, keep_alive_interval)
 logger.debug(f"Connected to MQTT broker at {broker_address}:{port_number}")
@@ -45,6 +46,7 @@ message_batcher = MessageBatcher(client, timeout)
 client.loop_start()
 
 try:
+    logger.info("Starting message batcher...")
     message_batcher.run()
 except KeyboardInterrupt:
     logger.info("Shutting down...")
